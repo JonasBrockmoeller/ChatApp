5c4f4d01dc8edee511e00e2d1a48ab4a0fe57580
Brocki <j.brockmoeller@student.fontys.nl>
2022-01-18 16:41:47 +0100

    bug fixing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,6 @@ module fynnandjonas.chatappfx {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
     exports fynnandjonas.chatappfx.UI;
     opens fynnandjonas.chatappfx.UI to javafx.fxml;
 }
