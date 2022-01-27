cd44a729e5f77ec46755c2e079a694e8e66acd50
Brocki <j.brockmoeller@student.fontys.nl>
2022-01-27 14:43:04 +0100

    first controller

---
+++ src/main/java/fynnandjonas/chatappfx/UI/MainScreenController.java
@@ -0,0 +1,18 @@
+package fynnandjonas.chatappfx.UI;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+
+public class MainScreenController {
+    public Button sendMessageButton;
+    public TextField messageField;
+    public ScrollPane scollPane;
+    public MenuItem changeServerSettingsMenu;
+    public MenuItem changeUsernameMenu;
+
+    public void sendMessage(ActionEvent actionEvent) {
+    }
+}
